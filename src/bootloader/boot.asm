c7c8f6e359b97246261910ab39bbe2dcbbbe87be
ox03bb <alibouakaz91@gmail.com>
2026-07-09 17:54:55 +0100

    update  boot files

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -3,24 +3,50 @@ bits 16
 
 jmp _start
 
-msg db "this is my bootLoader",0
+msg db 13, 10,"Welcom to my bootLoader", 13, 10
+    db "press any key to continue...", 0
+
+s_msg db 13,10,10,"starting...",0    
+
+
+
 
 _start:
+MOV bx, msg 
+MOV dx, 0  ;kernal start flag 
+
 
 print:
     MOV ah, 0x0E
-    MOV si, msg
+    MOV si, bx
     loop:
         MOV al, [si]
+        ; CMP al, '\n'  ; \ ascii code  
+        ; JE newline 
         CMP al, 0
-        JE loopend
+        JE .loop
 
         INT 0x10 
         INC si
         JMP loop
 
-    loopend:
+    .loop:
+    
      
+CMP dx,1
+JE end
+
+MOV ah, 0x00
+INT 0x16
+
+MOV bx, s_msg  ; Print starting msg
+MOV dx,1 ; Enable kernal start flag 
+JMP print
+
+
+end:
 jmp $
-times 510-($-$$) db 0
+
+
+times 510-($-$$) db 0 ;padding  
 db 0x55, 0xAA
--- test/boot.test.asm
+++ test/boot.test.asm
@@ -1,3 +1,27 @@
-jmp $
-times 510-($-$$) db 0
-db 0x55, 0xaa
+org 0x7C00
+bits 16
+
+start:
+    mov si, message
+
+.print:
+    lodsb               ; Load byte from [SI] into AL, SI++
+    test al, al         ; Is it the null terminator?
+    jz .hang
+
+    mov ah, 0x0E        ; BIOS teletype output
+    mov bh, 0x00        ; Display page
+    mov bl, 0x07        ; Text attribute (light gray on black)
+    int 0x10
+
+    jmp .print
+
+.hang:
+    jmp .hang
+
+HALT
+
+message db "Hello, World!", 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
