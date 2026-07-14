6c8131083e2a5f00de17ea29b53f4e19605fa801
ox03bb <alibouakaz91@gmail.com>
2026-07-14 14:54:53 +0100

    upddate req secript

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,6 +1,15 @@
 if [ -f /etc/arch-release ]; then
     sudo pacman -S --noconfirm --needed nasm qemu-full make git
+    yay --noconfirm  -S i686-elf-binutils i686-elf-gcc 
 else if [ -f /etc/debian_version ]; then
-    sudo apt-get install -y nasm qemu make git
+    sudo apt-get install -y nasm qemu make git\
+    build-essential \
+    bison \
+    flex \
+    libgmp3-dev \
+    libmpc-dev \
+    libmpfr-dev \
+    texinfo \
+    xz-utils
 fi
     
