feb20decdd8511d38569b9ca6d6d01e2e5506c79
Ox03bb <ali.de.bouakaz@gmail.com>
2026-08-20 00:55:51 +0100

    refactor vga module: enhance cursor handling and clear functionality

--- includes/vga.h
+++ includes/vga.h
@@ -1,5 +1,56 @@
 #ifndef VGA_H
 #define VGA_H
 
+#include <stdint.h>
+
+#define VGA_ADDRESS 0xB8000
+
+#define VGA_WIDTH  80
+#define VGA_HEIGHT 25
+#define VGA_RES  (VGA_HEIGHT * VGA_WIDTH)
+
+#define VGA_INDEX_PORT 0x3D4
+#define VGA_DATA_PORT  0x3D5
+
+#define VGA_CURSOR_HIGH 0x0E
+#define VGA_CURSOR_LOW  0x0F
+
+#define BLACK      0   // DARK_GRAY
+#define BLUE       1   // LIGHT_BLUE
+#define GREEN      2   // LIGHT_GREEN
+#define CYAN       3   // LIGHT_CYAN
+#define RED        4   // LIGHT_RED
+#define MAGENTA    5   // LIGHT_MAGENTA
+#define BROWN      6   // YELLOW
+#define LIGHT_GRAY 7   // WHITE
+
+#define LIGHT_FLAG 8
+
+typedef union vga_entry {
+    uint16_t value;
+
+    struct {
+        uint8_t character;
+        uint8_t attribute;
+    };
+};
+
+
+void vga_init(void);
+
+void vga_clear(void);
+
+void vga_put_char(char c);
+
+void vga_write(const char *str);
+
+void vga_set_color(uint8_t foreground, uint8_t background);
+
+void vga_set_cursor(uint8_t x, uint8_t y);
+
+uint8_t vga_get_cursor_x(void);
+
+uint8_t vga_get_cursor_y(void);
+
 
 #endif
--- src/kernal/driver/vga.c
+++ src/kernal/driver/vga.c
@@ -1,4 +1,31 @@
 #include "vga.h"
+#include "utils.h"
 
 
+
+
+void vga_set_cursor(uint8_t x, uint8_t y){
+    uint16_t p = y * VGA_WIDTH + x;
+
+    outb(VGA_INDEX_PORT, VGA_CURSOR_HIGH);
+    outb(VGA_DATA_PORT, (p >> 8 ));
+
+    outb(VGA_INDEX_PORT, VGA_CURSOR_LOW);
+    outb(VGA_DATA_PORT, p & 0xff );
+}
+
+
+
+void vga_clear(void){
+    volatile uint16_t *vga_buff = (uint16_t *)VGA_ADDRESS; 
+
+    vga_set_cursor(0, 0)
+
+    while( int i = 0 < VGA_RES; i ++){
+        vga_buff[i] = ' ' | (uint16_t(0) >> 8 )
+    }
+
+    vga_set_cursor(0, 0)
+
+}
 
