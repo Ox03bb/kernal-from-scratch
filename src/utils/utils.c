96db4839810927b8ebebb4eb7276c356a95873c1
Ox03bb <ali.de.bouakaz@gmail.com>
2026-08-29 11:52:39 +0100

    add some functions to utils

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -13,6 +13,19 @@ uint8_t inb(uint16_t port) {
     return value;
 }
 
+
+void cli(void){
+    __asm__ volatile("cli");
+}
+
+void sti(void) {
+    __asm__ volatile ("sti");
+}
+
+void hlt(void) {
+    __asm__ volatile ("hlt");
+}
+
 void io_wait(void) { // delay for I/O operation
 
     __asm__ volatile("outb %%al, $0x80" : : "a"(0));
